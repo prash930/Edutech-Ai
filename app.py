e9fa28b036b9b809544d86c7247a3adc3a964da0
Prashant Mhaske <119875508+prash930@users.noreply.github.com>
2026-04-21 02:06:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # ── API Key ───────────────────────────────────────────────────────────────────
-GROQ_API_KEY =os.getenv("GROQ_API_KEY")
+GROQ_API_KEY =st.getenv("GROQ_API_KEY")
 
 PAGES = ["🏠 Home", "🤖 AI Mentor", "📊 ROI Calculator", "🎯 Admission Predictor", "💰 Education Loan"]
 
