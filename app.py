50ea3dc1c207c1347833815403bb5dfdffe533f0
Prashant Mhaske <119875508+prash930@users.noreply.github.com>
2026-04-21 02:08:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,8 @@ import pandas as pd
 import json
 import time
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 # ── API Key ───────────────────────────────────────────────────────────────────
-GROQ_API_KEY =st.getenv("GROQ_API_KEY")
+GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 
 PAGES = ["🏠 Home", "🤖 AI Mentor", "📊 ROI Calculator", "🎯 Admission Predictor", "💰 Education Loan"]
 
